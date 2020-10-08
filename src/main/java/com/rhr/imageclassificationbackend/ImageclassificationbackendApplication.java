0c8b0a4f62f7ce4b9e7834d83434bfcafc2d48c9
Roland Salloum <roland.salloum00@outlook.com>
2020-10-08 20:16:24 +0300

    chore: adding initial back-end setup

---
+++ src/main/java/com/rhr/imageclassificationbackend/ImageclassificationbackendApplication.java
@@ -0,0 +1,13 @@
+package com.rhr.imageclassificationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageclassificationbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageclassificationbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rhr/imageclassificationbackend/ImageclassificationbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhr.imageclassificationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageclassificationbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
