d95cb433b3db84bcb134daf1570654eb1d478520
Roland Salloum <roland.salloum00@outlook.com>
2020-10-08 21:14:22 +0300

    feat: adding RestTemplate to receive API from python microservice

--- src/main/java/com/rhr/imageclassificationbackend/ImageclassificationbackendApplication.java
+++ src/main/java/com/rhr/imageclassificationbackend/ImageclassificationbackendApplication.java
@@ -2,9 +2,29 @@ package com.rhr.imageclassificationbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 public class ImageclassificationbackendApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+        messageConverters.add(converter);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(messageConverters);
+        return restTemplate;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ImageclassificationbackendApplication.class, args);
---
+++ src/main/java/com/rhr/imageclassificationbackend/model/Message.java
@@ -0,0 +1,12 @@
+package com.rhr.imageclassificationbackend.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+    private String message;
+}
