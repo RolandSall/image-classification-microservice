d53e1153b2e0a07fa3412041d2d8884b0b5b3978
Roland Salloum <roland.salloum00@outlook.com>
2020-10-08 21:22:56 +0300

    feat: adding RestTemplate to receive API from python microservice

--- src/main/java/com/rhr/imageclassificationbackend/controllers/Home.java
+++ src/main/java/com/rhr/imageclassificationbackend/controllers/Home.java
@@ -1,15 +1,29 @@
 package com.rhr.imageclassificationbackend.controllers;
 
+import com.rhr.imageclassificationbackend.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/")
 public class Home {
 
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public Home(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping
-    public String test(){
-        return "Hello";
+    public ResponseEntity<Message> test(){
+        ResponseEntity<Message> message = restTemplate.getForEntity("http://localhost:5000/", Message.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(message.getBody());
     }
 }
